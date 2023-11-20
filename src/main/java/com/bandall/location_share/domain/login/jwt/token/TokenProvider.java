f997f29fcb7cf029267da59f7b1e695efde1b7d3
bandall <jsm5315@naver.com>
2023-11-20 21:26:11 +0900

    refactor: ID+PW 로그인 로직 수정
    ID+PW 로그인 시 스프링 시큐리티의 비밀번호 비교 로직 대신 직접 비교하도록 변경

--- src/main/java/com/bandall/location_share/domain/login/jwt/token/TokenProvider.java
+++ src/main/java/com/bandall/location_share/domain/login/jwt/token/TokenProvider.java
@@ -2,9 +2,8 @@ package com.bandall.location_share.domain.login.jwt.token;
 
 import com.bandall.location_share.aop.LoggerAOP;
 import com.bandall.location_share.domain.login.jwt.dto.TokenInfoDto;
-import com.bandall.location_share.domain.member.UserPrinciple;
 import com.bandall.location_share.domain.member.Member;
-import com.bandall.location_share.domain.member.MemberDetails;
+import com.bandall.location_share.domain.member.UserPrinciple;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -14,7 +13,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
 
 import java.security.Key;
 import java.util.Arrays;
@@ -48,48 +46,7 @@ public class TokenProvider {
         this.blackListRepository = blackListRepository;
     }
 
-    // 토큰 생성 함수
-    public TokenInfoDto createTokenWithAuthentication(Authentication authentication) {
-        String authorities = authentication.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.joining(","));
-
-        long currentTime = (new Date()).getTime();
-        Date accessTokenExpireTime = new Date(currentTime + this.accessTokenValidationInMilliseconds);
-        Date refreshTokenExpireTime = new Date(currentTime + this.refreshTokenValidationInMilliseconds);
-        String email = ((MemberDetails) authentication.getPrincipal()).getEmail();
-        String tokenId = UUID.randomUUID().toString();
-
-        // Access 토큰
-        String accessToken = Jwts.builder()
-                .setSubject(email)
-                .claim(KEY_AUTHORITIES, authorities)
-                .claim(KEY_USERNAME, authentication.getName())
-                .claim(KEY_TOKEN_ID, tokenId)
-                .signWith(hashKey, SignatureAlgorithm.HS512)
-                .setExpiration(accessTokenExpireTime)
-                .compact();
-
-        // Refresh 토큰
-        String refreshToken = Jwts.builder()
-                .setExpiration(refreshTokenExpireTime)
-                .setSubject(email)
-                .claim(KEY_TOKEN_ID, tokenId)
-                .signWith(hashKey, SignatureAlgorithm.HS512)
-                .compact();
-
-        return TokenInfoDto.builder()
-                .ownerEmail(email)
-                .tokenId(tokenId)
-                .accessToken(accessToken)
-                .accessTokenExpireTime(accessTokenExpireTime)
-                .refreshToken(refreshToken)
-                .refreshTokenExpireTime(refreshTokenExpireTime)
-                .build();
-    }
-
-    // refresh 토큰으로 access 토큰 갱신 시 사용
-    public TokenInfoDto createTokenWithMember(Member member) {
+    public TokenInfoDto createToken(Member member) {
         long currentTime = (new Date()).getTime();
         Date accessTokenExpireTime = new Date(currentTime + this.accessTokenValidationInMilliseconds);
         Date refreshTokenExpireTime = new Date(currentTime + this.refreshTokenValidationInMilliseconds);
@@ -133,8 +90,8 @@ public class TokenProvider {
 
         Collection<? extends GrantedAuthority> authorities =
                 Arrays.stream(claims.get(KEY_AUTHORITIES).toString().split(","))
-                .map(SimpleGrantedAuthority::new)
-                .collect(Collectors.toList());
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
 
         // 커스텀한 UserPrinciple 객체 사용 -> 이후 추가적인 데이터를 토큰에 넣을 경우 UserPrinciple 객체 및 이 클래스의 함수들 수정 필요
         UserPrinciple principle = new UserPrinciple(claims.getSubject(), claims.get(KEY_USERNAME, String.class), authorities);
@@ -144,13 +101,13 @@ public class TokenProvider {
 
     // 토큰 유효성 검사 -> access, refresh 토큰 둘 다 검증하는 함수이므로 access 토큰 블랙리스트는 체크하지 않는다.
     public TokenValidationResult validateToken(String token) {
-        TokenValidationResult validResult = new TokenValidationResult(false, TokenType.ACCESS, null,null, null);
+        TokenValidationResult validResult = new TokenValidationResult(false, TokenType.ACCESS, null, null, null);
         Claims claims = null;
         try {
             claims = Jwts.parserBuilder().setSigningKey(hashKey).build().parseClaimsJws(token).getBody();
 
             // 토큰에 권한정보가 없을 경우 Refresh 토큰
-            if(claims.get(KEY_AUTHORITIES) == null) validResult.setTokenType(TokenType.REFRESH);
+            if (claims.get(KEY_AUTHORITIES) == null) validResult.setTokenType(TokenType.REFRESH);
 
             validResult.setResult(true);
             validResult.setTokenId(claims.get(KEY_TOKEN_ID, String.class));
@@ -165,7 +122,7 @@ public class TokenProvider {
         } catch (SecurityException | MalformedJwtException e) {
             log.info("잘못된 jwt 서명");
             validResult.setTokenStatus(TokenStatus.TOKEN_WRONG_SIGNATURE);
-        }  catch (UnsupportedJwtException e) {
+        } catch (UnsupportedJwtException e) {
             log.info("지원되지 않는 jwt 서명");
             validResult.setTokenStatus(TokenStatus.TOKEN_HASH_NOT_SUPPORTED);
         } catch (IllegalArgumentException e) {
@@ -182,27 +139,27 @@ public class TokenProvider {
         TokenValidationResult aTokenRes = validateToken(accessToken);
         TokenValidationResult totalResult = new TokenValidationResult(true, null, null, TokenStatus.TOKEN_VALID, null);
 
-        if(refTokenRes.getTokenStatus() == TokenStatus.TOKEN_EXPIRED) {
+        if (refTokenRes.getTokenStatus() == TokenStatus.TOKEN_EXPIRED) {
             log.info("Expired Refresh Token");
             totalResult.setTokenStatus(TokenStatus.TOKEN_EXPIRED);
             return totalResult;
         }
 
-        if(!refTokenRes.getResult()) {
+        if (!refTokenRes.getResult()) {
             log.info("Wrong Refresh Token");
             totalResult.setTokenStatus(TokenStatus.TOKEN_WRONG_SIGNATURE);
             return totalResult;
         }
 
         // 잘못된 access 토큰일 경우에만 => 재발급 할꺼라 만료된 것도 OK
-        if(!aTokenRes.getResult() && aTokenRes.getTokenStatus() != TokenStatus.TOKEN_EXPIRED) {
+        if (!aTokenRes.getResult() && aTokenRes.getTokenStatus() != TokenStatus.TOKEN_EXPIRED) {
             log.info("Wrong Access Token");
             totalResult.setTokenStatus(TokenStatus.TOKEN_WRONG_SIGNATURE);
             return totalResult;
         }
 
         // tokenId 쌍 검증
-        if(!refTokenRes.getTokenId().equals(aTokenRes.getTokenId())) {
+        if (!refTokenRes.getTokenId().equals(aTokenRes.getTokenId())) {
             log.info("Wrong refresh & access tokenId pair");
             totalResult.setTokenStatus(TokenStatus.TOKEN_ID_NOT_MATCH);
             return totalResult;
@@ -213,7 +170,7 @@ public class TokenProvider {
     }
 
     public boolean isAccessTokenBlackList(String accessToken) {
-        if(blackListRepository.isKeyBlackList(accessToken)) {
+        if (blackListRepository.isKeyBlackList(accessToken)) {
             log.info("폐기된 jwt 토큰");
             return true;
         } else {
